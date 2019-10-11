a6374a03c2413adda9d9179c292418b52d3b05eb
Andrei <andrei.gugeanu@3pillarglobal.com>
2019-10-11 15:54:34 +0300

    Add some style to Person component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Person.css";
 
 const person = props => {
   return (
-    <div>
+    <div className="Person">
       <p>
         I am {props.name}.I have {props.age} years old.
       </p>
